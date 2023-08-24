c30eecb8e087f340ee35acc77b0dd121341350ae
Jorge Cano <canomelero1@correo.ugr.es>
2023-08-24 15:42:17 +0200

    agregadas las fichas al tablero

--- js/main.js
+++ js/main.js
@@ -5,7 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const boton1 = document.querySelector('#un_jugador');
     const boton2 = document.querySelector('#dos_jugadores');
     const div_botones = document.querySelector('#botones');
+    const tablero = new Tablero();
+    const marca_x = "X";
+    const marca_o = "O";
     let juegoOFF = true;
+    let arrayTablero = [];
+    let turno1 = true;
+    
 
     
     // #### Eventos ####
@@ -17,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    // #### Funciones ####
+    // #### Funciones Principales ####
     function juegoUno(evento) {
         console.log(evento.target.id);
         juegoOFF = false;
@@ -36,17 +42,83 @@ document.addEventListener('DOMContentLoaded', () => {
         juegoOFF = false;
         div_botones.removeChild(div_botones.lastChild);
 
-        const tablero = new Tablero();
-
         console.log(tablero);
 
+        rellenarArray();
+        console.log(arrayTablero)
+        
+        //while(!acabarPartida()) {
+            for(let i = 0; i < arrayTablero.length; i++) {
+                arrayTablero[i].addEventListener('click', agregarFicha);
+            }
+        //}
     }
 
+
+    // #### Otras Funciones ####
     function alerta(mensaje) {
         const msg_alerta = document.createElement("p");
         msg_alerta.textContent = mensaje;
         msg_alerta.classList.add('alerta');
         div_botones.appendChild(msg_alerta);
+    }
+
+    function rellenarArray() {
+        for(let i = 1; i <= 9; i++) {
+            arrayTablero.push(document.querySelector(`#casilla${i}`));
+        }
+    }
+
+    function agregarFicha(evento) {
+        if(turno1) {
+            turno1 = false;
+            tablero.setCasilla(evento.target.title, marca_x);    // se agrega al objeto tablero
+            console.log(tablero);
+            agregarHTML();   // se agrega al tablero del HTML
+        }
+        else {
+            turno1 = true;
+            tablero.setCasilla(evento.target.title, marca_o);
+            console.log(tablero);
+            agregarHTML();
+        }
+    }
+
+    function agregarHTML() {
+        limpiarTableroHTML();
+
+        for(let i = 0; i < tablero.filas; i++) {
+            for(let j = 0; j < tablero.col; j++) {
+                if(tablero.getElemento(i, j) === marca_x || tablero.getElemento(i, j) === marca_o) {
+                    const msg = document.createElement("p");
+                    msg.textContent = tablero.getElemento(i, j);
+
+                    /*if(elemento === marca_x) {
+                        msg.classList.add('marca-x');
+                    }
+                    else {
+                        msg.classList.add('marca-o');
+                    }*/
+                    
+                    const casilla = tablero.getNumCasilla(i, j);
+
+                    const div_casilla = document.querySelector(`#casilla${casilla}`);
+                    div_casilla.appendChild(msg);
+                }
+            }
+        }
+    }
+
+    function limpiarTableroHTML() {
+        for(let i = 0; i <= arrayTablero.length; i++) {
+            const div = document.querySelector(`#casilla${i+1}`);
+            
+            if(div !== null) {
+                while(div.firstChild) {
+                    div.removeChild(div.firstChild);
+                }
+            }
+        }
     }
 });
 
--- js/tablero.js
+++ js/tablero.js
@@ -1,18 +1,57 @@
+const fil_default = 3;
+const col_default = 3;
 
 export class Tablero {
-    constructor(filas = 3, col = 3) {
+    // #### Inicialización de los datos ####
+    constructor(filas = fil_default, col = col_default) {
         this.filas = filas;
         this.col = col;
-    
+        
+        let num = 1;
         this.arrayTablero = new Array();
 
         for(let i = 0; i < filas; i++) {
             this.arrayTablero[i] = new Array();
 
             for(let j = 0; j < col; j++) {
-                this.arrayTablero[i][j] = "";
+                this.arrayTablero[i][j] = `${num}`;
+                num++;
             }
         }
     }
+
+    // #### Métodos ####
+    setCasilla(casilla, elemento) {
+        for(let i = 0; i < this.filas; i++) {
+            for(let j = 0; j < this.col; j++) {
+                if(this.arrayTablero[i][j] === casilla) {
+                    this.arrayTablero[i][j] = elemento;
+                }
+            }
+        }
+    }
+
+    getElemento(fila, columna) {
+        return this.arrayTablero[fila][columna];
+    }
+
+    getNumCasilla(fila, columna) {
+        let num_casilla = 1, i = 0, j = 0;
+        let encontrado = false;
+
+        for(let i = 0; i < this.filas && !encontrado; i++) {
+            for(let j = 0; j < this.col && !encontrado; j++) {
+                if(fila === i && columna === j) {
+                    encontrado = true;
+                }
+                else {
+                    num_casilla++;
+                }
+            }
+        }
+
+        return num_casilla;
+    }
+    
 
 }
